72bbf65d927092b367c7ebd3896be6346b82da01
Ebenezer Mensah <ebenezer2446@byui.edu>
2024-03-14 17:56:01 -0600

    Version 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,10 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        buildFeatures {
+            dataBinding = true
+        }
     }
 
     buildTypes {
@@ -46,12 +50,20 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/DEPENDENCIES"
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
+
 }
 
 buildscript {
     repositories {
+        google()
         mavenCentral()
     }
 }
@@ -62,7 +74,26 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     // Facebook Android SDK (everything)
-    implementation("com.facebook.android:facebook-android-sdk:latest.release")
+    implementation ("com.facebook.android:facebook-android-sdk:[8,9)")
+    //implementation ("com.facebook.android:facebook-android-sdk:16.3.0") // Use the latest version available
+    implementation ("com.facebook.android:facebook-login:latest.release")
+
+
+    implementation ("androidx.appcompat:appcompat:1.2.0")
+
+    implementation ("com.google.android.material:material:1.4.0")
+
+    implementation ("com.google.cloud:google-cloud-translate:2.0.1") // Use the latest version
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+    implementation ("androidx.camera:camera-camera2:1.0.0-beta07")
+    implementation ("androidx.camera:camera-lifecycle:1.0.0-beta07")
+    implementation ("androidx.camera:camera-view:1.0.0-alpha20")
+
+
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation ("androidx.core:core-ktx:1.6.0")
 
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
@@ -72,6 +103,10 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.play.services.mlkit.text.recognition)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/verboversa/ui/theme/SignUPActivity2.java
@@ -0,0 +1,6 @@
+package com.example.verboversa.ui.theme;
+
+import android.app.Activity;
+
+public class SignUPActivity2 extends Activity {
+}
